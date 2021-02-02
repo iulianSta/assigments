62fc13f8116a23cdd697241c668e439780b71bd3
iulianstan <iulian1226@gmail.com>
2021-02-02 14:31:14 +0100

    Exercise 5

--- index.js
+++ index.js
@@ -88,8 +88,35 @@ const countLetters = (str) => {
     {acc[cur] = acc[cur] 
         ? acc[cur] + 1 
         : 1;
-    return acc}, {});
+    return acc
+    }, {});
 
 }
 
 console.log(countLetters("tree"));
+
+// Ex. 5
+/* > Free Shipping. 
+Create a function that determines whether an online order should get free shipping. 
+An order gets free shipping if the total cost of items exceeds €50.
+
+Examples:
+
+freeShipping({ "Sponge": 3.50, "Soap": 5.99 }) ➞ false
+
+freeShipping({ "Surround Sound Equipment": 499.99 }) ➞ true
+
+freeShipping({ "Wool": 13.99, "Knitting Needles": 15.50, "Bag": 13.99 }) ➞ false */
+
+const freeShipping = (obj) => {
+   let sumOfValues = Object.values(obj).reduce((acc, cur) => acc + cur);
+   //return sumOfValues;
+   if (sumOfValues > 50) {
+       return true;
+   } else {
+       return false;
+   }
+}
+console.log(freeShipping({ "Sponge": 3.50, "Soap": 5.99 }));
+console.log(freeShipping({ "Surround Sound Equipment": 499.99 }));
+console.log(freeShipping({ "Wool": 13.99, "Knitting Needles": 15.50, "Bag": 13.99 }));
